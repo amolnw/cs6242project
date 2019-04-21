c5cd4729d13e2e2dd13f788d7fae4c38ad3fc270
Amol Nandkishor Wadwalkar <amolnw@hotmail.com>
2019-04-20 20:23:30 -0400

    Update azure-ml-fareamount-counts.js

--- DataContent/APIs/taxidataapi/azure-ml-fareamount-counts.js
+++ DataContent/APIs/taxidataapi/azure-ml-fareamount-counts.js
@@ -7,46 +7,9 @@ exports.handler =  (event, context, callback) => {
     console.log('event json is ' + JSON.stringify(event['json']));
     const params = {
         url: 'https://ussouthcentral.services.azureml.net/workspaces/a33bc535e638467295fcc065406e10e8/services/f6c0c516fd694b72b36284beed054c28/execute?api-version=2.0&details=true',
-        headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer Rxa7Qzzq0TbN+BdU6jBvHn4EBGXv0LuqGHcjyH6x24hRURUvffoQq1pYpn7jHQjAhc8//B1xPPPYUSO8eIGGGg==' },
+        headers: { 'Content-Type': 'application/json' },
         json: event
-    };    
-/*
-    const params = {
-        url: 'https://ussouthcentral.services.azureml.net/workspaces/a33bc535e638467295fcc065406e10e8/services/d9e9ca48b2ab4fdd9a87fdf0868c728e/execute?api-version=2.0&details=true',
-        headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer mFzMN1A+dJatX0rW4KoTXAZH+PyX8OPQH/92w+dK/gRALCo/i5+RMRLPUyBgT2cp4DO6swpLtQCNfj124k7a3g==' },
-        json: '{\
-            "Inputs": {\
-              "input1": {\
-                "ColumnNames": [\
-                  "latitude",\
-                  "longitude",\
-                  "weekday",\
-                  "hour"\
-                ],\
-                "Values": [\
-                  [\
-                    "39.01997",\
-                    "-62.15469",\
-                    "Wed",\
-                    "9"        ],\
-                  [\
-                    "40.57442",\
-                    "-74.00723",\
-                    "Sun",\
-                    "0"        ],\
-                  [\
-                    "40.57414",\
-                    "-73.98085",\
-                    "Tue",\
-                    "20"\
-                              ]\
-                ]\
-              }\
-            },\
-            "GlobalParameters": {}\
-          }'
-    };
-*/    
+    };     
     console.log('------before req call ------');
     try {
         console.log('------req ------' + JSON.stringify(params));
@@ -63,10 +26,6 @@ exports.handler =  (event, context, callback) => {
     }catch(err) {
         console.log('------ error calling  ------' + err);
     }
-
-//                    callback(pickup_error);
-//                    callback(null,pickup_results);
-
 };
   
 
